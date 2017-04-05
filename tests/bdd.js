6452fd23cb89ff89f7a5fe9eea60781555de713d
Michael Herman <hermanmu@gmail.com>
2017-04-05 07:51:24 -0600

    added mocha bdd example

---
+++ tests/bdd.js
@@ -0,0 +1,51 @@
+process.env.NODE_ENV = 'test';
+
+const Selector = require('testcafe').Selector;
+const chai = require('chai');
+
+const should = chai.should();
+
+// selectors
+const title = Selector('h1');
+const tableRows = Selector('tbody > tr');
+const addJobButton = Selector('a.btn.btn-primary');
+const firstJob = Selector('tbody > tr').withText('Horse Whisperer');
+const submitButton = Selector('button[type="submit"]');
+const clayDryerJob = Selector('tbody > tr').withText('Clay Dryer');
+
+describe('mherman.org', () => {
+
+  before(() => {});
+  after(() => {});
+
+  describe('Blobs Resource', () => {
+    it('should list ALL blobs on /blobs GET');
+    it('should list a SINGLE blob on /blob/<id> GET');
+    it('should add a SINGLE blob on /blobs POST');
+    it('should update a SINGLE blob on /blob/<id> PUT');
+    it('should delete a SINGLE blob on /blob/<id> DELETE');
+  });
+
+  describe('Jobs Resource', () => {
+
+    fixture('Jobs Resource').page('http://localhost:3333');
+
+      it('should list ALL jobs on /jobs GET', (done) => {
+        test('All Jobs', async (t) => {
+          // check title, add job button, table rows, and job exists
+          await t
+            .expect(title.innerText).eql('All Jobs')
+            .expect(addJobButton.innerText).eql('Add New Job')
+            .expect(tableRows.count).eql(3)
+            .expect(firstJob.exists).ok();
+          done();
+        });
+      });
+      it('should list a SINGLE job on /job/<id> GET');
+      it('should add a SINGLE job on /jobs POST');
+      it('should update a SINGLE job on /job/<id> PUT');
+      it('should delete a SINGLE job on /job/<id> DELETE');
+
+  });
+
+});
